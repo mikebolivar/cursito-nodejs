cd6f885158fab68157aedfd833f2504a08fbfc8e
Miguel Bolivar <m87.bolivar@gmail.com>
2014-05-20 08:40:10 -0500

    Creemos metodos no bloqueantes, se respone desde los manipuladores

--- server.js
+++ server.js
@@ -4,12 +4,9 @@ var url = require("url");
 function iniciar(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log("Peticion para " + pathname + " recibida.");
+    console.log("Request for " + pathname + " received.");
 
-    response.writeHead(200, {"Content-Type": "text/html"});
-    var content = route(handle, pathname)
-    response.write(content);
-    response.end();
+    route(handle, pathname, response);
   }
 
   http.createServer(onRequest).listen(8888);
